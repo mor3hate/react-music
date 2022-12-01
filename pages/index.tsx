125c51b84b45535979a88c63ce8a77e440ae8248
Mikhail Zotin <thelokkedmail3@gmail.com>
2022-12-01 13:12:40 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export async function getStaticProps() {
 		const songs: ITopChartSong[] = songsData.slice(0,21).map((item, i) => ({
 			artists: item.artists || '',
 			hub: item.hub,
-			images: item.images || '/public/music.png',
+			images: item.images || '/music.png',
 			index: i,
 			key: item.key,
 			subtitle: item.subtitle,
