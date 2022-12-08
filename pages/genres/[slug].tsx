58e1af383a4c76c4ecdb22415d560c0bac2217aa
mor3hate <thelokkedmail3@gmail.com>
2022-12-09 02:43:52 +0300

    Fix(api version changed)

--- pages/genres/[slug].tsx
+++ pages/genres/[slug].tsx
@@ -36,7 +36,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 		const charts: ITopChartSong[] = chartsData.slice(0, 21).map((item, i) => ({
 			artists: item.artists || [],
 			hub: item.hub,
-			images: item.images,
+			images: item.images || '',
 			index: i,
 			key: item.key,
 			subtitle: item.subtitle,
